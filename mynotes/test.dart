1a34ccc80e1a6daae8e943b3c220d83556028ba3
Messi002 <rostandmessi2@gmail.com>
2023-02-22 18:02:31 +0100

    testing out some stuffs

--- mynotes/test.dart
+++ mynotes/test.dart
@@ -1,43 +1,19 @@
-import 'dart:convert';
-
-class DrinkData {
-  String day;
-  List<int> amountDrank;
-  
-
-  DrinkData({required this.day, required this.amountDrank});
-
-  factory DrinkData.fromJson(Map<String, dynamic> json) {
-    return DrinkData(
-      day: json['day'],
-      amountDrank: List<int>.from(json['amountDrank']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['day'] = day;
-    data['amountDrank'] = amountDrank;
-    return data;
-  }
-
-  @override
-  String toString() => 'day : $day, amountDrank : $amountDrank';
+extension ListExtension on List {
+  int get getAmountDrankLength =>
+      (this[0]['amountDrank'] as List?)?.length ?? 1;
 }
 
+
+
 void main(List<String> args) {
-  String jsonData =
-      '[{"day": "16/02/23", "amountDrank": [1, 3, 4, 5, 6]}, {"day": "17/02/23", "amountDrank": [1, 3, 4, 5, 6]}, {"day": "18/02/23", "amountDrank": [1, 3, 4, 5, 6]}]';
-      '[{"day": "16/02/23", "amountDrank": [1, 3, 4, 5, 6]}, {"day": "17/02/23", "amountDrank": [1, 3, 4, 5, 6]}, {"day": "18/02/23", "amountDrank": [1, 3, 4, 5, 6]}]';
+  var list = [
+    {
+      // 'date': '2023-02-20',
+      // 'amountDrank': [2]
+    }
+  ];
 
-  List<DrinkData> drinkDataList = (json.decode(jsonData) as List<dynamic>)
-      .map((dynamic json) => DrinkData.fromJson(json))
-      .toList();
+  List<int>? list2 = list[0]['amountDrank'] as List<int>? ?? [] ;
 
-  List<Map<String, dynamic>> dataAsMapList =
-      drinkDataList.map((data) => data.toJson()).toList();
-
-  print('${drinkDataList}');
-  print('---------------------------');
-  print('${dataAsMapList[1]['day'].runtimeType}');
+  print('This is the length ${list.getAmountDrankLength}');
 }
